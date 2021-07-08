69bbc52d68b83977e24d37756f04bd6fb776bd90
Tyler Richards <richardstylerj@yahoo.com>
2021-07-08 13:05:50 -0500

    Add files via upload

---
+++ setup/Requirements.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#fswebcam for picture taking
+sudo apt install fswebcam
+
+#boto3 for Amazon S3 access
+sudo pip3 install boto3
+
+#awscli for AWS utility
+sudo apt install awscli
+
+#periphery for SI7021
+sudo pip3 install python-periphery
+
+#pyserial for MHZ16
+sudo pip3 install pyserial
+
+#multiple python libraries for google sheet
+sudo pip3 install gspread
+sudo pip3 install google-api-python-client
+sudo pip3 install oauth2client
+
+#couchdb
+sudo pip3 install couchdb
