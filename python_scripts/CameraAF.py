11a4e65d768c81f8690e876207bda74ea6fe50fc
Tyler Richards <richardstylerj@yahoo.com>
2021-07-08 13:01:54 -0500

    Add files via upload

---
+++ python_scripts/CameraAF.py
@@ -0,0 +1,17 @@
+from picamera import PiCamera
+from time import sleep
+import datetime
+import os
+
+camera = PiCamera(resolution=(640,480))
+#camera.rotation = 180 # uncomment if picture is upside down
+# Set focus
+focus = 450 # 6 to 14 inches
+value = (focus<<4) & 0x3ff0
+dat1 = (value>>8)&0x3f
+dat2 = value & 0xf0
+os.system("i2cset -y 0 0x0c %d %d" % (dat1,dat2))
+
+sleep(2)
+camera.capture('/home/pi/MVP/pictures/' + format(datetime.datetime.now(), '%Y-%m-%d_%H%M') + '.jpg')
+camera.close()
