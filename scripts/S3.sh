8ae0fc2291a2c902fbacbd43c9fd546e89263008
Tyler Richards <richardstylerj@yahoo.com>
2021-07-15 13:09:25 -0500

    Update S3.sh

--- scripts/S3.sh
+++ scripts/S3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Telling the script where to find credentials
-AWS_CONFIG_FILE="~/.aws/config"
+AWS_CONFIG_FILE="/home/pi/Desktop/MarsFarmMini/.aws/config"
 
 python3 /home/pi/Desktop/MarsFarmMini/python_scripts/S3.py
