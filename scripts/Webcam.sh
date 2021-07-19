64e7c292754356f3318da9cf63ae76e351d65c20
Tyler Richards <richardstylerj@yahoo.com>
2021-07-19 13:56:18 -0500

    Fixed path

--- scripts/Webcam.sh
+++ scripts/Webcam.sh
@@ -2,4 +2,4 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-fswebcam -r 640x360 --skip 10 --no-banner --set frequency=60 /home/pi/MVP/pictures/$DATE.jpg
+fswebcam -r 640x360 --skip 10 --no-banner --set frequency=60 /home/pi/Desktop/MarsFarmMini/pictures/$DATE.jpg
