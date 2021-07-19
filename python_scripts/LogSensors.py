d7c12c6a1612b7203e9f17670a099d210afa240a
Tyler Richards <richardstylerj@yahoo.com>
2021-07-19 15:07:40 -0500

    Update LogSensors.py

--- python_scripts/LogSensors.py
+++ python_scripts/LogSensors.py
@@ -41,15 +41,15 @@ def log_sensors(test = False):
             co2 = get_co2(con)
 
         status = 'Success'
-        print(status)
-        print(co2)
+        #print(status)
+        #print(co2)
         if test:
             status = 'Test'
         insert_one(EnvironmentalObservation(OBSERVATION_DATE, 'co2', co2, 'ppm', TRIAL_ID, TRIAL_NAME, TRIAL_START_DATE))
 
     except Exception as e:
         status = 'Failure'
-        print(status)
+        #print(status)
         if test:
             status = 'Test'
     
@@ -59,15 +59,15 @@ def log_sensors(test = False):
         temp = si.get_tempC()
 
         status = 'Success'
-        print(status)
-        print(temp)
+        #print(status)
+        #print(temp)
         if test:
             status = 'Test'
         insert_one(EnvironmentalObservation(OBSERVATION_DATE, 'temperature', temp, 'C', TRIAL_ID, TRIAL_NAME, TRIAL_START_DATE))
         
     except Exception as e:
         status = 'Failure'
-        print(status)
+        #print(status)
         if test:
             status = 'Test'
 
@@ -77,15 +77,15 @@ def log_sensors(test = False):
         humid = si.get_humidity()
 
         status = 'Success'
-        print(status)
-        print(humid)
+        #print(status)
+        #print(humid)
         if test:
             status = 'Test'
         insert_one(EnvironmentalObservation(OBSERVATION_DATE, 'humidity', humid, '%', TRIAL_ID, TRIAL_NAME, TRIAL_START_DATE))
         
     except Exception as e:
         status = 'Failure'
-        print(status)
+        #print(status)
         if test:
             status = 'Test'
 
