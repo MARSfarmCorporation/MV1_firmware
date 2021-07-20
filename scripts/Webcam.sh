780514cf405adcedc77494d9f3c8c0c0b5fc2b41
Tyler Richards <richardstylerj@yahoo.com>
2021-07-20 11:10:27 -0500

    Updated path

--- scripts/Webcam.sh
+++ scripts/Webcam.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-DATE=$(date +"%Y-%m-%d_%H%M")
-
-fswebcam -r 640x360 --skip 10 --no-banner --set frequency=60 /home/pi/Desktop/MarsFarmMini/pictures/$DATE.jpg
+python3 /home/pi/Desktop/MarsFarmMini/python_scripts/CameraAF.py
+#DATE=$(date +"%Y-%m-%d_%H%M")
+#fswebcam -r 640x360 --skip 10 --no-banner --set frequency=60 /home/pi/Desktop/MarsFarmMini/pictures/$DATE.jpg
