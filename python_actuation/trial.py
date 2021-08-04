f833860649b18459169a81026ea6ac802ba06092
Tyler Richards <richardstylerj@yahoo.com>
2021-08-04 15:04:51 -0500

    Update trial.py

--- python_actuation/trial.py
+++ python_actuation/trial.py
@@ -34,7 +34,7 @@ trial = {
                  },
                  {
                      "start_time": [13, 25],
-                     "setting": 0
+                     "setting": 80
                  },
                  {
                      "start_time": [15, 40],
