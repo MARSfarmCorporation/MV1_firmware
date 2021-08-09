19f4160213f28cc0bfc7260adfad225d8ac85924
Tyler Richards <richardstylerj@yahoo.com>
2021-08-09 10:41:50 -0500

    Create SHTC3.py

---
+++ python_scripts/SHTC3.py
@@ -0,0 +1,56 @@
+"""
+SI7021 humidity and temperature sensor
+Technical notes of commands and operation and from:
+https://www.silabs.com/documents/public/data-sheets/Si7021-A20.pdf
+
+ Author : Howard Webb
+ Date   : 06/20/2018
+
+"""
+
+import time
+from I2CUtil import I2C, bytesToWord
+
+# Device I2C address
+addr = 0x70
+read_command = 0x7866
+
+
+class SHTC3(object):
+
+    def __init__(self):
+        self._addr = addr
+        self._i2c = I2C(addr)
+
+    def read_data(self):
+        # Reads both humidity and temp at the same time
+
+        temp = None
+        humidity = None
+
+        msgs = self._i2c.msg_write([read_command])
+
+        # need a pause here between sending the request and getting the data
+        time.sleep(0.013)
+        msgs = self._i2c.msg_read(6)
+
+        if msgs == None:
+            return None
+        else:
+            temp_data = msgs[0:2]
+            humidity_data = msgs[3:5]
+
+        # decode data into human values:
+        # convert bytes into 16-bit signed integer
+        # convert the LSB value to a human value according to the datasheet
+        raw_temp = (temp_data[1] << 8) + temp_data[0]
+        raw_temp = ((4375 * raw_temp) >> 14) - 4500
+        temperature = raw_temp / 100.0
+
+        # repeat above steps for humidity data
+        raw_humidity = (humidity_data[1] << 8) + humidity_data[0]
+        raw_humidity = (625 * raw_humidity) >> 12
+        humidity = raw_humidity / 100.0
+
+        return [temperature, humidity]
+
