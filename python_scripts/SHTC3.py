62dd2641a9654c2dc376a233ab329e94783853cf
Tyler Richards <richardstylerj@yahoo.com>
2021-08-09 11:45:00 -0500

    Update SHTC3.py

--- python_scripts/SHTC3.py
+++ python_scripts/SHTC3.py
@@ -36,18 +36,16 @@ class SHTC3(object):
         if msgs == None:
             return None
         else:
-            temp_data = [bytesToWord(msgs[0].data[0]), bytesToWord(msgs[0].data[1])]
-            humidity_data = [bytesToWord(msgs[0].data[3]), bytesToWord(msgs[0].data[4])]
+            raw_temp = bytesToWord(msgs[0].data[0], msgs[0].data[1])
+            raw_humidity = bytesToWord(msgs[0].data[3], msgs[0].data[4])
 
         # decode data into human values:
         # convert bytes into 16-bit signed integer
         # convert the LSB value to a human value according to the datasheet
-        raw_temp = (temp_data[1] << 8) + temp_data[0]
         raw_temp = ((4375 * raw_temp) >> 14) - 4500
         temperature = raw_temp / 100.0
 
         # repeat above steps for humidity data
-        raw_humidity = (humidity_data[1] << 8) + humidity_data[0]
         raw_humidity = (625 * raw_humidity) >> 12
         humidity = raw_humidity / 100.0
         
