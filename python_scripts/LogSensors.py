2f34622ba47f070c7c232a5334def388fb024dcd
Tyler Richards <richardstylerj@yahoo.com>
2021-08-09 11:03:30 -0500

    New sensor to LogSensors

--- python_scripts/LogSensors.py
+++ python_scripts/LogSensors.py
@@ -1,15 +1,14 @@
 """
-Log standard MVP sensors
+Log standard MARSfarm Mini sensors
 Please run LogSensors.py OWITH PYTHON3!!!!!
 -Specifically, the CO2 collection part, or MHZ16.py, will only work in Python3
 -If want to run in Python2, get rid of all parts related to CO2 collection
-    
-Author: Howard Webb 
-Modified by: Jackie Zhong(zy99120@gmail.com), Henry Borska - July 2021 - (HLB948@gmail.com), Tyler Richards(richardstylerj20@gmail.com)
+
+Author: Tyler Richards
 """
 
 from Remote_MongoUtil import EnvironmentalObservation, insert_one
-from SI7021 import *
+from SHTC3 import *
 from GSheetUtil import update_sheet
 from MHZ16 import get_co2
 import serial
@@ -21,19 +20,14 @@ import time
 TRIAL_NAME = "Test Trial 1"
 TRIAL_ID = "60db520a1a0f29f5a74a0f62"
 TRIAL_START_DATE = 1623621469
-OBSERVATION_DATE = time.time() #change to date now 
-ATTRIBUTE = "humidity"
-UNIT = "%"
-VALUE = 106
-
+OBSERVATION_DATE = time.time() #change to date now
 
 def log_sensors(test = False):
-    
-    
 
-    si=SI7021()
+
+    sht=SHTC3()
     con = serial.Serial("/dev/ttyAMA0", 9600, timeout=5)
- 
+
     #Inserting co2 (ppm) into database
     try:
         co2 = get_co2(con)
@@ -47,48 +41,19 @@ def log_sensors(test = False):
             status = 'Test'
         insert_one(EnvironmentalObservation(OBSERVATION_DATE, 'co2', co2, 'ppm', TRIAL_ID, TRIAL_NAME, TRIAL_START_DATE))
 
-    except Exception as e:
-        status = 'Failure'
-        #print(status)
-        if test:
-            status = 'Test'
-    
-
-    #Inserting temperature (C) into database
+    #Inserting humidity and temp into database
     try:
-        temp = si.get_tempC()
+        sensor_data = sht.read_data()
+        temp2 = sensor_data[0]
+        humid2 = sensor_data[1]
 
-        status = 'Success'
-        #print(status)
-        #print(temp)
-        if test:
-            status = 'Test'
-        insert_one(EnvironmentalObservation(OBSERVATION_DATE, 'temperature', temp, 'C', TRIAL_ID, TRIAL_NAME, TRIAL_START_DATE))
-        
-    except Exception as e:
-        status = 'Failure'
-        #print(status)
-        if test:
-            status = 'Test'
-
-
-    #Inserting humidity (%) into database
-    try:
-        humid = si.get_humidity()
-
-        status = 'Success'
+        # insert_one(EnvironmentalObservation(OBSERVATION_DATE, 'humidity', humid, '%', TRIAL_ID, TRIAL_NAME, TRIAL_START_DATE))
+        # insert_one(EnvironmentalObservation(OBSERVATION_DATE, 'temperature', temp, 'C', TRIAL_ID, TRIAL_NAME, TRIAL_START_DATE))
         #print(status)
         #print(humid)
-        if test:
-            status = 'Test'
-        insert_one(EnvironmentalObservation(OBSERVATION_DATE, 'humidity', humid, '%', TRIAL_ID, TRIAL_NAME, TRIAL_START_DATE))
-        
-    except Exception as e:
-        status = 'Failure'
-        #print(status)
-        if test:
-            status = 'Test'
 
+    except Exception as e:
+        print("fault")
 
     #Update google sheets
     #this part is for google sheet update
@@ -96,14 +61,14 @@ def log_sensors(test = False):
         update_sheet('Environment_Observation', 'CO2', co2, 'ppm')
     except Exception as e:
         update_sheet('Environment_Observation_ERROR', 'CO2', 0, 'ppm')
-    
+
     try:
-        update_sheet('Environment_Observation', 'Temperature', temp, 'Celcius')
+        update_sheet('Environment_Observation', 'Temperature', temp2, 'Celcius')
     except Exception as e:
         update_sheet('Environment_Observation_ERROR', 'Temperature', '0', 'Celcius')
-    
+
     try:
-        update_sheet('Environment_Observation', 'Humidity', humid, 'Percentage')
+        update_sheet('Environment_Observation', 'Humidity', humid2, 'Percentage')
     except Exception as e:
         update_sheet('Environment_Observation_ERROR', 'Humidity', 0, 'Percentage')
 
@@ -111,6 +76,6 @@ def test():
     log_sensors(True)
 
 if __name__=="__main__":
-    log_sensors()    
+    log_sensors()
 
 
