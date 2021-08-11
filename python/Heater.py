4077fd0d1a72533b9410d2c8d3848a199f00b6a5
Tyler Richards <richardstylerj@yahoo.com>
2021-08-11 17:17:29 -0500

    Update Heater.py

--- python/Heater.py
+++ python/Heater.py
@@ -5,12 +5,12 @@ pi = pigpio.pi()
 class Heater:
     def __init__(self, gpio_pin):
         self.gpio = gpio_pin #Store heater GPIO
-        pi.set_PWM_frequency(self.gpio,1000) #Set heater as 1kHz PWM channel
+        pi.set_PWM_frequency(self.gpio,30) #Set heater as 1kHz PWM channel
         pi.set_PWM_dutycycle(self.gpio,0) #Turn off heater when initialized
         
     #Turn on and off heater
     def setState(self, state):
         if (state > 0):
-            pi.set_PWM_dutycycle(self.gpio,100) #PWM heater at 100
+            pi.set_PWM_dutycycle(self.gpio,50) #PWM heater at 100
         if (state == 0):
             pi.set_PWM_dutycycle(self.gpio,0) #PWM heater at 0
