475dd37f160c4cf62b4980fa3417fed98f7c8ef9
Tyler Richards <richardstylerj@yahoo.com>
2021-08-12 13:13:31 -0500

    Update Fan.py

--- python/Fan.py
+++ python/Fan.py
@@ -6,7 +6,7 @@ class Fan:
     def __init__(self, gpio_pin):
         self.gpio = gpio_pin #Store fan GPIO
         pi.set_mode(self.gpio, pigpio.OUTPUT) #Set fan as output
-        pi.write(self.gpio,0) #Turn off fan when initialized
+        #pi.write(self.gpio,0) #Turn off fan when initialized
         
     #Turn on and off fan
     def setState(self, state):
