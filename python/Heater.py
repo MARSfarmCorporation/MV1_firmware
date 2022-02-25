5bbda086060910c89798c1dc489776000add2413
Tyler Richards <richardstylerj@yahoo.com>
2022-02-25 15:41:51 -0600

    Updated for Buck Circuit

--- python/Heater.py
+++ python/Heater.py
@@ -5,12 +5,12 @@ pi = pigpio.pi()
 class Heater:
     def __init__(self, gpio_pin):
         self.gpio = gpio_pin #Store heater GPIO
-        pi.set_PWM_frequency(self.gpio,50000) #Set heater as 1kHz PWM channel
+        pi.set_PWM_frequency(self.gpio,70000) #Set heater as 70kHz PWM channel
         pi.set_PWM_dutycycle(self.gpio,0) #Turn off heater when initialized
         
     #Turn on and off heater
     def setState(self, state):
         if (state > 0):
-            pi.set_PWM_dutycycle(self.gpio,155) #PWM heater (ratio of 126/255: 50W)
+            pi.set_PWM_dutycycle(self.gpio,50) #PWM heater (ratio of 126/255: 50W)
         if (state == 0):
             pi.set_PWM_dutycycle(self.gpio,0) #PWM heater at 0
