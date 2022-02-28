8d3aaefbf130d4a15c59455b1178af16b06f4714
Tyler Richards <richardstylerj@yahoo.com>
2022-02-28 10:39:13 -0600

    Updated PWM frequency

--- python/Heater.py
+++ python/Heater.py
@@ -5,7 +5,7 @@ pi = pigpio.pi()
 class Heater:
     def __init__(self, gpio_pin):
         self.gpio = gpio_pin #Store heater GPIO
-        pi.set_PWM_frequency(self.gpio,70000) #Set heater as 70kHz PWM channel
+        pi.set_PWM_frequency(self.gpio,40000) #Set heater as 70kHz PWM channel
         pi.set_PWM_dutycycle(self.gpio,0) #Turn off heater when initialized
         
     #Turn on and off heater
