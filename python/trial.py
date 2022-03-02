a94775dd786b6b11fa90df6559628f6c938c5d13
Tyler Richards <richardstylerj@yahoo.com>
2022-03-02 11:16:51 -0600

    Test

--- python/trial.py
+++ python/trial.py
@@ -1,73 +1,213 @@
-trial = {
+{
     "_id": {
-        "$oid": "60db520a1a0f29f5a74a0f62"
+        "$oid": "6176e6e1c12a7e4a97ef7136"
     },
-    "device_id": "60f1a223786cb0a4a6a5a175",
-    "start_date": 1628007453,
-    "start_date_str": "2021-07-01 00:41",
-    "model_name": "Trial",
-    "recipe": [
-        {"recipe_id": "60f1cfcd1c80bff2be8d4a94",
-         "recipe_name": "marsfarm_photoperiod_14/10",
-         "recipe_variable": "photoperiod"
-         }
-    ],
+    "model_name": "TemplateRecipe",
+    "recipe_name": "FFA_Test_Recipe_V1",
+    "recipe_variable": "Light and Temperature",
     "phases": [
-        {"name": "growth",
-         "phase_start": 0,
-         "step": [
-            {"temperature": [
-                 {
-                     "start_time": [0, 0],
-                     "setting": 75
-                 },
-                 {
-                     "start_time": [11, 40],
-                     "setting": 85
-                 }
-             ]
-             },
-            {"light_intensity": [
-                 {
-                     "start_time": [0, 0],
-                     "setting": [0, 0, 0, 0, 0]
-                 },
-                 {
-                     "start_time": [12, 0],
-                     "setting": [75, 75, 75, 75, 75]
-                 }
-             ]
-             }
-         ]
-
-         },
-        {"name": "veg",
-         "phase_start": 28,
-         "step": [
-            {"temperature": [
-                 {
-                     "start_time": [0, 0],
-                     "setting": [75]
-                 },
-                 {
-                     "start_time": [16, 0],
-                     "setting": [85]
-                 }
-             ]
-             },
-            {"light_intensity": [
-                 {
-                     "start_time": [0, 0],
-                     "setting": [0, 0, 0, 0, 0]
-                 },
-                 {
-                     "start_time": [10, 0],
-                     "setting": [75, 75, 75, 75, 75]
-                 }
-             ]
-             }
-         ]
-
-         }
+        {
+            "name": "growth",
+            "phase_start": 0,
+            "step": [
+                {
+                    "circulation_fan": [
+                        {
+                            "start_time": [
+                                0,
+                                0
+                            ],
+                            "setting": 1
+                        }
+                    ]
+                },
+                {
+                    "temperature": [
+                        {
+                            "start_time": [
+                                0,
+                                0
+                            ],
+                            "setting": 85
+                        }
+                    ]
+                },
+                {
+                    "light_intensity": [
+                        {
+                            "start_time": [
+                                0,
+                                0
+                            ],
+                            "setting": [
+                                0,
+                                0,
+                                0,
+                                0
+                            ]
+                        },
+                        {
+                            "start_time": [
+                                9,
+                                0
+                            ],
+                            "setting": [
+                                0,
+                                200,
+                                80,
+                                0
+                            ]
+                        },
+                        {
+                            "start_time": [
+                                12,
+                                30
+                            ],
+                            "setting": [
+                                0,
+                                0,
+                                0,
+                                0
+                            ]
+                        },
+                        {
+                            "start_time": [
+                                12,
+                                45
+                            ],
+                            "setting": [
+                                125,
+                                200,
+                                200,
+                                100
+                            ]
+                        },
+                        {
+                            "start_time": [
+                                12,
+                                55
+                            ],
+                            "setting": [
+                                0,
+                                0,
+                                0,
+                                0
+                            ]
+                        },
+                        {
+                            "start_time": [
+                                13,
+                                0
+                            ],
+                            "setting": [
+                                125,
+                                200,
+                                200,
+                                100
+                            ]
+                        },
+                        {
+                            "start_time": [
+                                14,
+                                15
+                            ],
+                            "setting": [
+                                0,
+                                0,
+                                0,
+                                0
+                            ]
+                        },
+                        {
+                            "start_time": [
+                                14,
+                                20
+                            ],
+                            "setting": [
+                                125,
+                                200,
+                                200,
+                                100
+                            ]
+                        },
+                        {
+                            "start_time": [
+                                17,
+                                0
+                            ],
+                            "setting": [
+                                0,
+                                0,
+                                0,
+                                0
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "veg",
+            "phase_start": 28,
+            "step": [
+                {
+                    "circulation_fan": [
+                        {
+                            "start_time": [
+                                0,
+                                0
+                            ],
+                            "setting": 1
+                        }
+                    ]
+                },
+                {
+                    "temperature": [
+                        {
+                            "start_time": [
+                                0,
+                                0
+                            ],
+                            "setting": 75
+                        },
+                        {
+                            "start_time": [
+                                16,
+                                0
+                            ],
+                            "setting": 85
+                        }
+                    ]
+                },
+                {
+                    "light_intensity": [
+                        {
+                            "start_time": [
+                                0,
+                                0
+                            ],
+                            "setting": [
+                                0,
+                                0,
+                                0,
+                                0
+                            ]
+                        },
+                        {
+                            "start_time": [
+                                10,
+                                0
+                            ],
+                            "setting": [
+                                75,
+                                75,
+                                75,
+                                75
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
     ]
 }
