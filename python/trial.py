d493786762867ab03943041a5b36ce329316b668
Tyler Richards <richardstylerj@yahoo.com>
2022-03-23 11:02:36 -0500

    Fixed trial.py

--- python/trial.py
+++ python/trial.py
@@ -1,105 +1,238 @@
 trial = {
-    "_id": {
-        "$oid": "60db520a1a0f29f5a74a0f62"
-    },
-    "trial_name": "Temperature and Light Testing",
-    "device_id": "60f1a223786cb0a4a6a5a175",
-    "start_date": 1628007453,
-    "start_date_str": "2021-07-01 00:41",
-    "model_name": "Trial",
-    "recipe": [
-        {"recipe_id": "60f1cfcd1c80bff2be8d4a94",
-         "recipe_name": "marsfarm_photoperiod_14/10",
-         "recipe_variable": "photoperiod"
-         }
-    ],
-    "phases": [
-        {"name": "growth",
-         "phase_start": 0,
-         "step": [
-            {"circulation_fan": [
-                 {
-                     "start_time": [0, 0],
-                     "setting": 1
-                 }
-             ]
+   "_id":"621fa7e8ac211300166be205",
+   "model_name":"TemplateRecipe",
+   "recipe_name":"FFA_Test_Recipe_V1",
+   "recipe_variable":"Light and Temperature",
+   "phases":[
+      {
+         "name":"growth",
+         "phase_start":0,
+         "step":[
+            {
+               "circulation_fan":[
+                  {
+                     "start_time":[
+                        0,
+                        0
+                     ],
+                     "setting":1
+                  }
+               ]
             },
-            {"temperature": [
-                 {
-                     "start_time": [0, 0],
-                     "setting": 85
-                 }
-             ]
-             },
-            {"light_intensity": [
-                 {
-                     "start_time": [0, 0],
-                     "setting": [0, 0, 0, 0]
-                 },
-                 {
-                    "start_time": [9, 0],
-                    "setting": [126, 200, 200, 100]
-                 },
-                 {
-                    "start_time": [12, 30],
-                    "setting": [0, 0, 0, 0]
-                 },
-                 {
-                    "start_time": [18, 15],
-                    "setting": [125, 200, 200, 100]
-                 },
-                 {
-                    "start_time": [19, 15],
-                    "setting": [0, 0, 0, 0]
-                 },
-                 {
-                    "start_time": [20, 15],
-                    "setting": [125, 200, 200, 100]
-                 },
-                 {
-                    "start_time": [21, 15],
-                    "setting": [0, 0, 0, 0]
-                 },
-                 {
-                    "start_time": [22, 30],
-                    "setting": [125, 200, 200, 100]
-                 },
-                 {
-                    "start_time": [23, 15],
-                    "setting": [0, 0, 0, 0]
-                 }
-             ]
-             }
+            {
+               "temperature":[
+                  {
+                     "start_time":[
+                        0,
+                        0
+                     ],
+                     "setting":90
+                  }
+               ]
+            },
+	    {
+               "pump_amount":[
+                  {
+                     "start_time":[
+                        11,
+                        10
+                     ],
+                     "setting":20
+                  }
+               ]
+            },
+            {
+               "light_intensity":[
+                  {
+                     "start_time":[
+                        0,
+                        0
+                     ],
+                     "setting":[
+                        0,
+                        0,
+                        0,
+                        0
+                     ]
+                  },
+                  {
+                     "start_time":[
+                        9,
+                        0
+                     ],
+                     "setting":[
+                        0,
+                        200,
+                        80,
+                        0
+                     ]
+                  },
+                  {
+                     "start_time":[
+                        12,
+                        30
+                     ],
+                     "setting":[
+                        0,
+                        0,
+                        0,
+                        0
+                     ]
+                  },
+                  {
+                     "start_time":[
+                        12,
+                        45
+                     ],
+                     "setting":[
+                        125,
+                        200,
+                        200,
+                        100
+                     ]
+                  },
+                  {
+                     "start_time":[
+                        12,
+                        55
+                     ],
+                     "setting":[
+                        0,
+                        0,
+                        0,
+                        0
+                     ]
+                  },
+                  {
+                     "start_time":[
+                        13,
+                        0
+                     ],
+                     "setting":[
+                        125,
+                        200,
+                        200,
+                        100
+                     ]
+                  },
+                  {
+                     "start_time":[
+                        14,
+                        15
+                     ],
+                     "setting":[
+                        0,
+                        0,
+                        0,
+                        0
+                     ]
+                  },
+                  {
+                     "start_time":[
+                        14,
+                        20
+                     ],
+                     "setting":[
+                        125,
+                        200,
+                        200,
+                        100
+                     ]
+                  },
+                  {
+                     "start_time":[
+                        17,
+                        0
+                     ],
+                     "setting":[
+                        0,
+                        0,
+                        0,
+                        0
+                     ]
+                  }
+               ]
+            }
          ]
-
-         },
-        {"name": "veg",
-         "phase_start": 28,
-         "step": [
-            {"temperature": [
-                 {
-                     "start_time": [0, 0],
-                     "setting": 75
-                 },
-                 {
-                     "start_time": [16, 0],
-                     "setting": 85
-                 }
-             ]
-             },
-            {"light_intensity": [
-                 {
-                     "start_time": [0, 0],
-                     "setting": [0, 0, 0, 0]
-                 },
-                 {
-                     "start_time": [10, 0],
-                     "setting": [75, 75, 75, 75]
-                 }
-             ]
-             }
+      },
+      {
+         "name":"veg",
+         "phase_start":28,
+         "step":[
+            {
+               "circulation_fan":[
+                  {
+                     "start_time":[
+                        0,
+                        0
+                     ],
+                     "setting":1
+                  }
+               ]
+            },
+            {
+               "temperature":[
+                  {
+                     "start_time":[
+                        0,
+                        0
+                     ],
+                     "setting":75
+                  },
+                  {
+                     "start_time":[
+                        16,
+                        0
+                     ],
+                     "setting":85
+                  }
+               ]
+            },
+	    {
+               "pump_amount":[
+                  {
+                     "start_time":[
+                        11,
+                        10
+                     ],
+                     "setting":20
+                  }
+               ]
+            },
+            {
+               "light_intensity":[
+                  {
+                     "start_time":[
+                        0,
+                        0
+                     ],
+                     "setting":[
+                        0,
+                        0,
+                        0,
+                        0
+                     ]
+                  },
+                  {
+                     "start_time":[
+                        10,
+                        0
+                     ],
+                     "setting":[
+                        75,
+                        75,
+                        75,
+                        75
+                     ]
+                  }
+               ]
+            }
          ]
-
-         }
-    ]
+      }
+   ],
+   "recipe_id":"6176e6e1c12a7e4a97ef7136",
+   "trial_name":"test_trial_03.02.2022",
+   "device_id":"60f1a223786cb0a4a6a5a175",
+   "device_name":"Tyler_test_device_08022021",
+   "start_date":1646241768.229
 }
