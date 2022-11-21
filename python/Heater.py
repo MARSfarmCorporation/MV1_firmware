1b5216099f89580684be58dbeb8086be57eb8ddb
WebbPeter <Webb.Peter.A@gmail.com>
2022-11-21 14:01:12 -0600

    Heater Sample Rate
    Increase sample rate of pigpio, when created by cron, so that it is fast enough for heater and will prevent ringing sound.

--- python/Heater.py
+++ python/Heater.py
@@ -1,8 +1,10 @@
 '''
 class representation of the heater
 This could be variable temperature, but here is either off or on
+pigpio must be set to sample rate of 1 microsecond in order to provide speed required for efficient use of Heater, done by cron now
 Author: Tyler Richards - 08.10.2021
 Modified By: Howard Webb - 11/2/2022
+Modified By: Peter Webb w/advice from Tyler Richards - 11/21/2022
 '''
 from pigpio import pi
 from GPIO_Conf import HEATER, ON, OFF
@@ -13,7 +15,8 @@ pi = pi()
 class Heater:
     def __init__(self):
         # Create heater in off state
-        pi.set_PWM_frequency(HEATER,40000) #Set heater as 70kHz PWM channel
+        We have to change sample rate to 1 microsecond so that it is able to provide a 40 KHz signal (faster switching of the heater)
+        pi.set_PWM_frequency(HEATER,40000) #Set heater as 40kHz PWM channel 
         pi.set_PWM_dutycycle(HEATER,OFF) #Turn off heater when initialized
        
     def on(self):
@@ -25,7 +28,9 @@ class Heater:
         
 def test():
     print("Test Heater")
+    print("PWM frequency BEFORE initialization", pi.get_PWM_frequency(HEATER))
     h = Heater()
+    print("PWM frequency AFTER initialization", pi.get_PWM_frequency(HEATER))
     print("Turn On")
     h.on()
     sleep(5)
@@ -35,4 +40,3 @@ def test():
 
 if __name__=="__main__":
     test()
-
