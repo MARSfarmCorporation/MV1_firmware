90bd82f970f417bc788af73d1a9d8e692da02774
WebbPeterA <webb.peter.a@gmail.com>
2023-02-22 10:08:19 -0600

    removed device ID, Google Sheet ID from config file and updated MAC address to new static range for batch 1

--- python/Sys_Conf.py
+++ python/Sys_Conf.py
@@ -8,13 +8,16 @@ Mostly directory paths
 #- Provisioning Date:
 #- Image Name:
 
-
 #VARIABLES SET DURING PRODUCTION PROVISIONING
 
 #Used for MQTT only - EVERYWHERE ELSE SHOULD USE Trial_Util.py
-DEVICE_ID = '60f1a223786cb0a4a6a5a175'
-#Used for Remote_GoogleSheet.py
-GOOGLE_SHEET_ID = '1MbRqOJNc0r9TPLMbF1aurRuqOKW9dOkEYR2dR2dP5Ps'
+# ADD THIS TO S3 AS WELL
+ DEVICE_ID = ''
+# Example - DEVICE_ID = '60f1a223786cb0a4a6a5a175'
+# Used for Remote_GoogleSheet.py
+ GOOGLE_SHEET_ID = ''
+# Example GOOGLE_SHEET_ID = '1MbRqOJNc0r9TPLMbF1aurRuqOKW9dOkEYR2dR2dP5Ps'
+
 #TBD ----- NOT WORKING YET Used for Ethernet Connection - may change this in cron instead
 #MAC_ADDRESS = 
 
