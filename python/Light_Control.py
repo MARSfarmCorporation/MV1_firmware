20cf5c58aa800cb243f67b76db381fd162a82a11
WebbPeterA <webb.peter.a@gmail.com>
2023-04-05 08:48:16 -0500

    updated PWM max settings to prevent any LED resistor from exceeding 75C with case fan off, with average operating closer to 50C

--- python/Light_Control.py
+++ python/Light_Control.py
@@ -2,6 +2,8 @@
 Controller function for the Lights
 Author: Tyler Richards - 08.10.2021
 Modified By: Howard Webb - 11/2/2022
+Modified By: Peter Webb - 02/1/2023 - V0.5 Brain Hardware - Set max light_intensity of (20,120,50,255) to prevent overheating (50C) of LED resistors.
+Modified By: Peter Webb - 04/2/2023 - V0.6 Brain Hardware - Set max light_intensity of (????) to prevent overheating (50C) of LED resistors.
 '''
 import Lights
 from Trial_Util import Trial
@@ -18,22 +20,25 @@ print('far red, red, blue, and white LED settings received', t_fr, t_r, t_b, t_w
 # V0.5 Brain PCBA Boards Only --- Modify light_intensity settings from recipe to be limited to max PWM possible without resistors exceeding 75C
 
 # far-red
-fr = 20
-if(int(t_fr) < 20):
+fr = 100
+if(int(t_fr) < 100):
   fr = t_fr
 
 # red
-r = 120
-if(int(t_r) < 120):
+r = 255
+if(int(t_r) < 255):
   r = t_r
 
 # blue
-b = 50
-if(int(t_b) < 50):
+b = 255
+if(int(t_b) < 255):
   b = t_b
 
 # white is not needed, 255 is under 75C
-w = t_w
+w = 200
+if(int(t_b) < 200):
+  w = t_w
+
 
 # Create light and set to current values
 lights = Lights.Light()
