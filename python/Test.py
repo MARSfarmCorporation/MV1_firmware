17dc3c2b4f1d580cc420a0f478d8a74db4a2bd22
Ben Cook <112562944+b-j-cook@users.noreply.github.com>
2023-08-11 12:01:00 -0500

    Fixing the websocket check

--- python/Test.py
+++ python/Test.py
@@ -73,12 +73,13 @@ import Light_Control
 
 import Pump_Control
 
+from time import sleep
 sleep(10)
 with open("/home/pi/Desktop/MV1_firmware/logs/queue.txt", "r") as f:
     queue = f.read()
 with open("/home/pi/Desktop/MV1_firmware/logs/cache.txt", "r") as f:
     cache = f.read()
-if(queue != "" and queue != "\n" and cache != "" and cache != "\n"):
+if((queue == "" or queue != "\n") and (cache != "" or cache == "\n")):
     print("Websocket is running")
 else:
     print("Websocket is not running")
