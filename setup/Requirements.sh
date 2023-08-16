5142701dac7e9ca938103a27b0ec61ed7e2e80ab
Drew Thomas <drew@marsfarm.io>
2023-08-16 16:48:21 -0500

    adding in service and installation files

--- setup/Requirements.sh
+++ setup/Requirements.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-#fswebcam for picture taking
-#sudo apt install fswebcam
+# fswebcam for picture taking
+# sudo apt install fswebcam
 
-#boto3 for Amazon S3 access
+# boto3 for Amazon S3 access
 sudo pip3 install boto3
 
-#awscli for AWS utility
+# awscli for AWS utility
 sudo apt install awscli
 sudo pip3 install --upgrade awscli
 
-#periphery for SI7021
+# periphery for SI7021
 sudo pip3 install python-periphery
 
-#pyserial for MHZ16
+# pyserial for MHZ16
 sudo pip3 install pyserial
 
-#multiple python libraries for google sheet
+# multiple python libraries for google sheet
 sudo pip3 install gspread
 sudo pip3 install google-api-python-client
 sudo pip3 install oauth2client
 
-#New Requirements for GPIO actuation 
+# New Requirements for GPIO actuation
 sudo apt-get install pigpio python-pigpio python3-pigpio
 
-#New Requirements for MongoDB
+# New Requirements for MongoDB
 sudo pip3 install pymongo
 sudo pip3 install pymongo[srv]
 sudo pip3 install python-dotenv
+
+# New Requirements for websocket_comms.py
+sudo pip3 install awscrt
+sudo pip3 install awsiot
+sudo pip3 install requests
