0817c56ef674bc7513ddcdb9df9e09d7a08c6184
Drew Thomas <drew@marsfarm.io>
2023-11-09 18:18:06 -0600

    Fix writing payload to Job_Agent_Log.txt in Broker.py

--- python/Broker.py
+++ python/Broker.py
@@ -22,8 +22,8 @@ job_notify_topic = f'$aws/things/{SERIAL_NUMBER}/jobs/notify-next'
 def spawn_job_agent(id, payload):
     try:
         # write the payload to Job_Agent_Log.txt, on a new line each time (make sure the file is there), with the prefix "Broker.py: "
-        #with open('../logs/Job_Agent_Log.txt', 'a') as file:
-        #    file.write(f"Broker.py: {payload}\n")
+        with open('../logs/Job_Agent_Log.txt', 'a') as file:
+            file.write(f"Broker.py: {payload}\n")
 
         # Start the Job_Agent.py process and pass the payload
         result = subprocess.run(['python3', 'Job_Agent.py', payload],
