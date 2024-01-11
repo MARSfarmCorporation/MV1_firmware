18e564822e96cd2f6613e94952ba38fe8b0a8a71
Drew Thomas <drew@marsfarm.io>
2024-01-11 12:33:56 -0600

    turns interface back on regardless of fail

--- python/Ethernet.py
+++ python/Ethernet.py
@@ -13,25 +13,24 @@ This file now also brings down the interface and then back up to ensure the MAC 
 import subprocess
 import datetime
 from Sys_Conf import ETH_MAC_ADDRESS, WLAN_MAC_ADDRESS
-from Lights import Light  # Replace 'your_light_module' with the actual name of your module containing the Light class
+from your_light_module import Light  # Replace 'your_light_module' with the actual name of your module containing the Light class
 
 def change_mac(interface, mac_address):
     # Take down the interface
     subprocess.run(['sudo', 'ifconfig', interface, 'down'])
-    
-    # Change the MAC address
-    cmd = f'sudo macchanger --mac={mac_address} {interface}'
+
     try:
+        # Change the MAC address
+        cmd = f'sudo macchanger --mac={mac_address} {interface}'
         subprocess.run(cmd.split(), check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print(f'Success: Changed MAC for {interface} at {datetime.datetime.now()}')
     except subprocess.CalledProcessError as e:
         print(f'Error: Failed to change MAC for {interface} at {datetime.datetime.now()} - {e.stderr}')
         light = Light()
         light.mac_fail()  # Call the mac_fail function from Light class
-        return None
-
-    # Bring up the interface
-    subprocess.run(['sudo', 'ifconfig', interface, 'up'])
+    finally:
+        # Bring up the interface regardless of previous success or failure
+        subprocess.run(['sudo', 'ifconfig', interface, 'up'])
 
 # Instantiate the Light class
 light = Light()
@@ -39,6 +38,7 @@ light = Light()
 # Change MAC addresses
 change_mac('eth0', ETH_MAC_ADDRESS)
 change_mac('wlan0', WLAN_MAC_ADDRESS)
+
 
 
 #Return light to current setting
