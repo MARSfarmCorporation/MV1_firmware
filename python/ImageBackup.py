2b863214aab499ce21e3f6e6575bbf4910ff9886
Drew Thomas <drew@marsfarm.io>
2024-06-14 17:56:02 -0500

    Step 2 of image backup testing

--- python/ImageBackup.py
+++ python/ImageBackup.py
@@ -3,19 +3,17 @@ import glob
 import boto3
 from Sys_Conf import DEVICE_ID, IMAGE_DIR, S3_BUCKET
 
-def list_image_names():
-    list_of_image_names = glob.glob(IMAGE_DIR + '*.jpg')
-    return list_of_image_names
+# Gets the list of image names in the local directory for comparison
+def list_local_image_names():
 
-# Get the list of image names from the S3 bucket
-def get_s3_image_names():
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(S3_BUCKET)
-    s3_image_names = []
-    for obj in bucket.objects.filter(Prefix=DEVICE_ID):
-        s3_image_names.append(obj.key)
-    return s3_image_names
-
+    local_image_names = []
+    # Get a list of files in the specified directory
+    list_of_files = glob.glob(IMAGE_DIR + '*')
+    # Get the name of the file
+    for file in list_of_files:
+        local_image_names.append(os.path.basename(file))
+    return local_image_names
+    
 
 def list_images_in_s3(bucket_name, prefix=''):
     """
@@ -39,10 +37,35 @@ def list_images_in_s3(bucket_name, prefix=''):
     
     return image_names
 
+def compare_image_lists(local_image_name_list, S3_image_name_list):
+    """
+    Compare local image names with S3 image names and return a list of images
+    that exist locally but not in S3.
+
+    :param local_image_name_list: List of local image names.
+    :param S3_image_name_list: List of image names in S3.
+    :return: List of image names that are local but not in S3.
+    """
+    # Convert S3_image_name_list to a set for faster lookups
+    S3_image_name_set = set(S3_image_name_list)
+    
+    # List comprehension to get local images not in S3
+    images_to_upload = [image for image in local_image_name_list if image not in S3_image_name_set]
+    
+    return images_to_upload
+
 if __name__ == "__main__":
     bucket_name = S3_BUCKET  # Replace with your S3 bucket name
     prefix = DEVICE_ID  # Replace with your prefix if needed
-    image_names = list_images_in_s3(bucket_name, prefix)
-    print(f"Found {len(image_names)} images in S3 bucket '{bucket_name}':")
-    for image_name in image_names:
-        print(image_name)
+    
+    # Get lists of image names
+    S3_image_name_list = list_images_in_s3(bucket_name, prefix)
+    local_image_name_list = list_local_image_names()
+    
+    # Compare lists
+    images_to_upload = compare_image_lists(local_image_name_list, S3_image_name_list)
+    
+    # Print result
+    print(f"Images to upload ({len(images_to_upload)}):")
+    for image in images_to_upload:
+        print(image)
