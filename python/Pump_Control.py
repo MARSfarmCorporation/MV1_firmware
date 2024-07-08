f06798f10bd59a391760190c1ded4fb941469aff
Drew Thomas <drew@marsfarm.io>
2024-07-08 12:18:27 -0500

    Pump testing 2

--- python/Pump_Control.py
+++ python/Pump_Control.py
@@ -4,7 +4,7 @@ Author: Tyler Richards - 05.10.2022
 Modified By: Howard Webb - 11/2/2022
 '''
 import os
-import time
+import time as time_module  # Renamed to avoid conflict
 from Trial_Util import Trial
 from Pump import Pump
 from datetime import datetime
@@ -30,7 +30,7 @@ pump_lock_timeout = 750  # 750 seconds = 12.5 minutes
 t = Trial()
 
 # Get current time
-time = datetime.now().strftime("%y-%m-%d %H:%M:%S")
+current_time = datetime.now().strftime("%y-%m-%d %H:%M:%S")  # Renamed to avoid conflict
 observation_date = datetime.now().timestamp()
 
 # creating an instance of the pump class
@@ -41,17 +41,17 @@ if os.path.exists(pump_lock_file):
     with open(pump_lock_file, 'r') as file:
         timestamp = float(file.read())
     # If the lock file is older than the timeout, ignore it
-    if time.time() - timestamp < pump_lock_timeout:
+    if time_module.time() - timestamp < pump_lock_timeout:
         # delete this after testing
         with open(pump_log_file, 'a') as file:
-            file.write(time + " - Lock file exists and is valid. Skipping scheduled task.\n")
+            file.write(current_time + " - Lock file exists and is valid. Skipping scheduled task.\n")
         print("Lock file exists and is valid. Skipping scheduled task.")
         exit()
 
 # check to verify that pump is not already pumping to prevent overwrite
 if p.is_pumping():
    print('Pump is already Pumping')
-   exit
+   exit()
 
 # retrieve pump settings from trial
 ps = t.get_pump_setting()
@@ -60,8 +60,8 @@ p.dispense(ps) # dispensing specified amount of water
 if (ps > 0):
 
     # Creating the payload via the enqueue function
-    devicedata_enqueue(mqtt_topic,"pump", ps, "mL", observation_date, "PumpObservation")
-    print('Pump dispersed ', ps, ' ML of water on ', time)
+    devicedata_enqueue(mqtt_topic, "pump", ps, "mL", observation_date, "PumpObservation")
+    print('Pump dispersed ', ps, ' ML of water on ', current_time)
 
 def test_pump(amount):
     p = Pump()
@@ -78,8 +78,8 @@ def test_pump(amount):
 
     if amount > 0:
         # Creating the payload via the enqueue function
-        devicedata_enqueue(mqtt_topic,"pump", amount, "mL", observation_date, "PumpObservation")
-        print('Test pump dispersed', amount, 'ML of water. This is likely due to user input from the Web Application, but could aslo be due to a test.')
+        devicedata_enqueue(mqtt_topic, "pump", amount, "mL", observation_date, "PumpObservation")
+        print('Test pump dispersed', amount, 'ML of water. This is likely due to user input from the Web Application, but could also be due to a test.')
         # delete this after testing
         with open(pump_log_file, 'a') as file:
-            file.write(time + " - Test pump dispersed " + str(amount) + " mL of water.\n")
+            file.write(current_time + " - Test pump dispersed " + str(amount) + " mL of water.\n")
