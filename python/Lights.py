aff50f38995174626476a09e45427b71f721500c
Drew Thomas <drew@marsfarm.io>
2024-07-09 18:20:30 -0500

    spelling error 567

--- python/Lights.py
+++ python/Lights.py
@@ -86,10 +86,10 @@ class Light:
     def trial_received_fail(self, num=3, spd=0.25):
         self.blink(num, spd, 0, 0, 100, 0)
     
-    def wifi_credtials_success(self, num=5, spd=0.25):
+    def wifi_credentials_success(self, num=5, spd=0.25):
         self.blink(num, spd, 0, 0, 0, 100)
 
-    def wifi_credtials_fail(self, num=5, spd=0.25):
+    def wifi_credentials_fail(self, num=5, spd=0.25):
         self.blink(num, spd, 0, 0, 100, 0)
 
     def blink(self, num=5, spd=1, fr=0, r=0, b=0, w=100):
