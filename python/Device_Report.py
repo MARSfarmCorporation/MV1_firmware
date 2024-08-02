5f8541f450d2d05e40c58e9bef570684cb022775
Drew Thomas <drew@marsfarm.io>
2024-08-02 14:01:34 -0500

    Commented wrong line

--- python/Device_Report.py
+++ python/Device_Report.py
@@ -52,8 +52,8 @@ def main():
     interface = get_connection_method()
     if interface != 'None':
         ip_address = get_ip_address(interface)
-        ssid = get_ssid(interface) #if interface == 'wlan0' else 'Not applicable'
-        visible_ssids = get_visible_ssids(interface) if interface == 'wlan0' else ['Not applicable']
+        ssid = get_ssid(interface) if interface == 'wlan0' else 'Not applicable'
+        visible_ssids = get_visible_ssids(interface) #if interface == 'wlan0' else ['Not applicable']
         
         # Write the network information and firmware version to the database to send to the MongoDB device record
         topic = f"device-report/{DEVICE_ID}"
