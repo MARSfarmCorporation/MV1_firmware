520ec61b7f826a844dda02f7d6cd418c0ff779fc
Drew Thomas <drew@marsfarm.io>
2024-08-13 15:10:19 -0500

    Testing logging of heater pin check

--- python/Heater.py
+++ python/Heater.py
@@ -1,14 +1,7 @@
-'''
-class representation of the heater
-This could be variable temperature, but here is either off or on
-pigpio must be set to sample rate of 1 microsecond in order to provide speed required for efficient use of Heater, done by cron now
-Author: Tyler Richards - 08.10.2021
-Modified By: Howard Webb - 11/2/2022
-Modified By: Peter Webb w/advice from Tyler Richards - 11/21/2022
-'''
 from pigpio import pi
 from GPIO_Conf import HEATER, ON, OFF, HEATER_PWM
 from time import sleep
+
 # pi needs to be defined outside the class
 # set up a connection to the pigpio daemon
 pi = pi()
@@ -16,31 +9,35 @@ pi = pi()
 # define a class for the heater
 class Heater:
     def __init__(self):
-        # intialize the heater in off state
-        #We have to change sample rate to 1 microsecond so that it is able to provide a 40 KHz signal (faster switching of the heater)
-        pi.set_PWM_frequency(HEATER,40000) #Set heater as 40kHz PWM channel 
-        pi.set_PWM_dutycycle(HEATER,OFF) #Turn off heater when initialized
-       
+        # initialize the heater in off state
+        #We have to change sample rate to 1 microsecond so that it is able to provide a 40 kHz signal (faster switching of the heater)
+        pi.set_PWM_frequency(HEATER, 40000)  # Set heater as 40kHz PWM channel
+        pi.set_PWM_dutycycle(HEATER, OFF)  # Turn off heater when initialized
+
     def on(self):
-       #turn on PWM heater (ratio of 126/255: 50W)
-       pi.set_PWM_dutycycle(HEATER,HEATER_PWM)
-       
+        # turn on PWM heater (ratio of 126/255: 50W)
+        pi.set_PWM_dutycycle(HEATER, HEATER_PWM)
+
     def off(self):
-       # turn off the heater 
-       pi.set_PWM_dutycycle(HEATER, OFF)
+        # turn off the heater
+        pi.set_PWM_dutycycle(HEATER, OFF)
+
+    def is_on(self):
+        # Check the actual physical state of the heater pin
+        return pi.read(HEATER) == 1  # Returns True if the pin is high (heater is on)
 
 # tests the heater's functions        
 def test():
     print("Test Heater")
     print("PWM frequency BEFORE initialization", pi.get_PWM_frequency(HEATER))
-    h = Heater() # create an instance of the heater class
+    h = Heater()  # create an instance of the heater class
     print("PWM frequency AFTER initialization", pi.get_PWM_frequency(HEATER))
     print("Turn On")
     h.on()
-    sleep(5) # wait for 5 seconds
+    sleep(5)  # wait for 5 seconds
     print("Turn Off")
     h.off()
     print("Done")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     test()
--- python/Thermostat.py
+++ python/Thermostat.py
@@ -8,50 +8,64 @@ import time
 from datetime import datetime
 
 class Thermostat(object):
-    
+
     def __init__(self):
         self.tu = Trial()
         self.efan = Exhaust_Fan()
         self.cfan = Circulation_Fan()
         self.heater = Heater()
-        
+
     def get_temp(self):
         s = SHTC3()
         temperature, humidity = s.get_tempF_humidity()
         return temperature
-    
+
     def get_Setpoint(self):
         t_s = self.tu.get_setpoint()
         s = 90
-        if(int(t_s) < 90):
-          s = t_s
+        if int(t_s) < 90:
+            s = t_s
         return s
-        
+
     def adjust(self):
         # Control code
         setpoint = self.get_Setpoint()
         temp = self.get_temp()
         print("Set", setpoint, "Temp", temp)
-        self.cfan.on() # Set circ fan state
+        self.cfan.on()  # Set circ fan state
         print("Circ_Fan: ON")
-        if ( temp < setpoint): #Measured temp is below setpoint
-            self.heater.on() #Turn on heater to raise temp   
-            print("Heater: On")
 
-        if ( temp >= setpoint): #Measured temp is above setpoint
-            self.heater.off() #Turn off heater to lower temp
+        if temp < setpoint:  # Measured temp is below setpoint
+            self.heater.on()  # Turn on heater to raise temp
+            print("Heater: On")
+        else:  # Measured temp is above setpoint
+            self.heater.off()  # Turn off heater to lower temp
             print("Heater: OFF")
 
+            # Take multiple readings of the heater pin after attempting to turn it off
+            self.check_heater_state(samples=100)
+
         # Exhaust fan control code
-        if ( temp >= setpoint + 1): #Measured temp is above setpoint by too much
-            self.efan.on() #Turn on exhaust fan
+        if temp >= setpoint + 1:  # Measured temp is above setpoint by too much
+            self.efan.on()  # Turn on exhaust fan
             print("Exhaust Fan ON")
         else:
-            self.efan.off() #Turn off fan
+            self.efan.off()  # Turn off fan
             print("Exhaust Fan OFF")
 
+    def check_heater_state(self, samples=100, delay=0.01):
+        # Check the state of the heater pin over a series of samples
+        for i in range(samples):
+            if self.heater.is_on():
+                self.log_error("Heater unexpectedly on during off period.")
+                break  # Log the error and exit early if an issue is detected
+            time.sleep(delay)  # Small delay between readings
 
-# testing the thermostat
+    def log_error(self, message):
+        # Log an error message with a timestamp
+        print(f"{datetime.now().isoformat()}: {message}\n")
+
+# Testing the thermostat
 def test():
     print("Thermostat Test")
     t = Thermostat()
@@ -59,8 +73,6 @@ def test():
     t.adjust()
     print(datetime.now().isoformat())
     print("Done")
-    
-if __name__=='__main__':
-    test()
-  
 
+if __name__ == "__main__":
+    test()
