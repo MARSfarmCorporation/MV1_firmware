1cbf7ae968611c519e5e7fa7710f953b259099d6
Drew Thomas <drew@marsfarm.io>
2024-08-14 19:39:19 -0500

    Revert "adding functionality to test rebooting if temp is over 100, and restarting pigpio if pin 21 gets stuck"
    This reverts commit e8530cb19e57ea195f78ef2868429bd5e34fe092.

--- python/Thermostat.py
+++ python/Thermostat.py
@@ -6,7 +6,6 @@ from Trial_Util import Trial
 from GPIO_Conf import ON, OFF
 import time
 from datetime import datetime
-import subprocess
 
 class Thermostat(object):
 
@@ -28,18 +27,7 @@ class Thermostat(object):
             s = t_s
         return s
 
-    def check_overheat_and_reboot(self):
-        """Check if the temperature exceeds 100°F and reboot the device if necessary."""
-        temp = self.get_temp()
-        if temp > 100:
-            print(f"Temperature is {temp}°F, exceeding 100°F! Rebooting device...")
-            subprocess.run(["sudo", "reboot"])
-            time.sleep(10)  # Pause to allow the reboot to start
-
     def adjust(self):
-        # Check if the temperature exceeds the threshold and reboot if necessary
-        self.check_overheat_and_reboot()
-
         # Control code
         setpoint = self.get_Setpoint()
         temp = self.get_temp()
@@ -63,24 +51,7 @@ class Thermostat(object):
                 time.sleep(0.1)
 
                 if self.heater.is_on():
-                    print("Warning: Heater is still on after reset! Killing, restarting, and reinitializing pigpio daemon...")
-
-                    # Find the PID of pigpiod
-                    pid = subprocess.check_output(["pgrep", "pigpiod"]).strip().decode('utf-8')
-                    
-                    # Kill the pigpiod process
-                    subprocess.run(["sudo", "kill", pid])
-                    
-                    # Restart pigpiod with the -s1 option
-                    subprocess.run(["sudo", "pigpiod", "-s1"])
-                    
-                    # Reinitialize the heater and fans after restarting pigpio
-                    self.heater = Heater()
-                    self.efan = Exhaust_Fan()
-                    self.cfan = Circulation_Fan()
-
-                    print("pigpio daemon restarted and peripherals reinitialized.")
-
+                    print("Warning: Heater is still on after reset!")
                 else:
                     print("Heater reset successfully, resuming normal operation...")
 
